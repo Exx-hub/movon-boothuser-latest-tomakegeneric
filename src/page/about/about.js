fc17de782d4d4b438408d06dfb349402a84e93bc
Alvin Acosta <alvin.acosta@movon.com.ph>
2022-05-23 14:16:13 +0800

    about page description update

--- src/page/about/about.js
+++ src/page/about/about.js
@@ -13,16 +13,13 @@ function About() {
         <div className="build-div">version {config.version.build}</div>
       </div>
       <div className="about-text">
-        Nisi laborum esse officia aliqua fugiat anim eu culpa. Ex aute voluptate
-        cupidatat aute incididunt quis eu sint. Nostrud excepteur exercitation
-        nulla sunt proident est aliquip proident occaecat. Mollit incididunt
-        adipisicing mollit ad incididunt ea do quis. Eiusmod duis occaecat duis
-        ipsum. Proident excepteur deserunt occaecat adipisicing. Nisi laborum
-        esse officia aliqua fugiat anim eu culpa. Ex aute voluptate cupidatat
-        aute incididunt quis eu sint. Nostrud excepteur exercitation nulla sunt
-        proident est aliquip proident occaecat. Mollit incididunt adipisicing
-        mollit ad incididunt ea do quis. Eiusmod duis occaecat duis ipsum.
-        Proident excepteur deserunt occaecat adipisicing.
+        MovOn Booth User is a web app created to handle walk in client
+        transactions. It is designed to accommodate those who wish to reserve
+        seats in advance or select and purchase trips from bus terminals. Booth
+        User App is also an alternative to using MovOn's mobile app, for getting
+        guaranteed seats. Bus partners can also easily track daily reports for
+        transactions and ticket purchases whether done online through the mobile
+        app or done via this web app for walk-in transactions.
       </div>
       <div className="about-footer">
         <span>
